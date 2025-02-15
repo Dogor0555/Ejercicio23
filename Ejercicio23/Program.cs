06d3cdfda96380c7d43d3ee620e53b5f416706da
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:46:32 -0600

    Ejercicio23

--- Ejercicio23/Program.cs
+++ Ejercicio23/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa el primer número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa el segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int suma = num1 + num2;
+        double promedio = suma / 2.0;
+
+        Console.WriteLine("El promedio es: " + promedio);
+    }
+}
